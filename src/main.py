fbb005a8180c3fa5f1daf52631203089db7e76a7
Haroun AlMasalmeh <haroun.greekbase@gmail.com>
2023-09-11 09:37:40 -0300

    Use pre-trained model

--- src/main.py
+++ src/main.py
@@ -24,9 +24,9 @@ if __name__ == "__main__":
 
     train_generator, validation_generator, test_generator = build_features(training_data_path, testing_data_path)
 
-    model = train_model(model_name, train_generator, validation_generator)
+    # model = train_model(model_name, train_generator, validation_generator)
 
-    # custom_objects = {'F1Score': tfa.metrics.F1Score(num_classes=1, average='micro', threshold=0.5)}
-    # model = load_model(model_name, custom_objects=custom_objects)
+    custom_objects = {'F1Score': tfa.metrics.F1Score(num_classes=1, average='micro', threshold=0.5)}
+    model = load_model(model_name, custom_objects=custom_objects)
     test_loss, test_f1 = predict_model(model, test_generator)
     print(f'Loss: {test_loss:.4f}, F1_Score: {test_f1:.4f}')
